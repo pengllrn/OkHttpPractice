65ab2c960c6c2939b57074921e88f8edf7ef6365
pengllrn <1256880407@qq.com>
2017-09-28 10:01:13 +0800

    新增导航栏，引入gson，优化parseJson代码

---
+++ app/src/main/java/com/pengllrn/okhttppractice/dataClass/All.java
@@ -0,0 +1,32 @@
+package com.pengllrn.okhttppractice.dataClass;
+
+import java.util.List;
+
+/**
+ * @author Administrator
+ * @version $Rev$
+ * @des ${UTODO}
+ * @updateAuthor ${Author}$
+ * @updateDate2017/9/27.
+ */
+
+public class All {
+    private List<Device> device;
+    private List<SchoolById> schoolbyid;
+
+    public List<Device> getDevice() {
+        return device;
+    }
+
+    public void setDevice(List<Device> device) {
+        this.device = device;
+    }
+
+    public List<SchoolById> getSchoolbyid() {
+        return schoolbyid;
+    }
+
+    public void setSchoolyyid(List<SchoolById> schoolbyid) {
+        this.schoolbyid = schoolbyid;
+    }
+}
---
+++ app/src/main/java/com/pengllrn/okhttppractice/dataClass/Room.java
@@ -0,0 +1,21 @@
+package com.pengllrn.okhttppractice.dataClass;
+
+/**
+ * @author Administrator
+ * @version $Rev$
+ * @des ${UTODO}
+ * @updateAuthor ${Author}$
+ * @updateDate2017/9/27.
+ */
+
+public class Room {
+    private String roomname = "";
+
+    public String getRoomname() {
+        return roomname;
+    }
+
+    public void setRoomname(String roomname) {
+        this.roomname = roomname;
+    }
+}
